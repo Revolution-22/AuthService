cca8132ed453bb92b9479ba16d49983f3f5fb593
karolwolny <kwolny31@gmail.com>
2025-03-17 18:47:54 +0100

    Remove public

--- src/test/groovy/com/revolution/auth/service/domain/adapters/TestBrokerService.java
+++ src/test/groovy/com/revolution/auth/service/domain/adapters/TestBrokerService.java
@@ -2,7 +2,7 @@ package com.revolution.auth.service.domain.adapters;
 
 import com.revolution.auth.service.api.port.BrokerService;
 
-public class TestBrokerService implements BrokerService {
+class TestBrokerService implements BrokerService {
 
     @Override
     public void publishMessage(String topic, Object message) {
