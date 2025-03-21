a293c9aa8226d3bb9ad6221ebddc5b6acf5d8419
karolwolny <kwolny31@gmail.com>
2025-03-21 13:47:41 +0100

    Fixes from code review

--- src/main/java/com/revolution/auth/service/infrastructure/configuration/KafkaTopicConfig.java
+++ src/main/java/com/revolution/auth/service/infrastructure/configuration/KafkaTopicConfig.java
@@ -1,32 +1,23 @@
 package com.revolution.auth.service.infrastructure.configuration;
 
-import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaAdmin;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static com.revolution.auth.service.api.Topics.REGISTER_TOPIC;
 
 @Configuration
 class KafkaTopicConfig {
 
-    @Value(value = "${spring.kafka.bootstrap-servers}")
-    private String bootstrapAddress;
-
     @Bean
-    KafkaAdmin kafkaAdmin() {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        return new KafkaAdmin(configs);
+    KafkaAdmin kafkaAdmin(KafkaProperties kafkaProperties) {
+        return new KafkaAdmin(kafkaProperties.buildAdminProperties(null));
     }
     
     @Bean
-    NewTopic topic1() {
+    NewTopic registerUserTopic() {
         return new NewTopic(REGISTER_TOPIC, 1, (short) 1);
     }
 }
